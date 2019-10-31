8c9a244fcc782d1eecbed83b3db85aa5061aa206
You Hye Ji <gpwl0773@gmail.com>
2019-10-31 23:35:15 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pochi_000\desktop\likelion\중앙\django_api\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
